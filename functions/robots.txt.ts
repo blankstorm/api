2679028e17f52479a751200b0826b9105b01f5e5
James Prevett <jp@jamespre.dev>
2025-01-02 17:13:32 -0600

    Fixed robots.txt

--- functions/robots.txt.ts
+++ functions/robots.txt.ts
@@ -1,12 +1,24 @@
-import { StatusCodes } from 'http-status-codes';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import type { RequestContext } from '../src/backend/context';
-import { error, response } from '../src/backend/utils';
 
 export async function onRequest({ env }: RequestContext) {
 	try {
-		return response(StatusCodes.OK, 'User-agent: *\nDisallow: /', false);
-	} catch (e: any) {
-		console.error(e);
-		return error(StatusCodes.INTERNAL_SERVER_ERROR, env.DEBUG && e?.message);
+		return new Response('User-agent: *\nDisallow: /', {
+			status: StatusCodes.OK,
+			statusText: ReasonPhrases.OK,
+			headers: {
+				'access-control-allow-origin': '*',
+				'content-type': 'text/plain; charset=utf-8',
+			},
+		});
+	} catch {
+		return new Response(null, {
+			status: StatusCodes.OK,
+			statusText: ReasonPhrases.OK,
+			headers: {
+				'access-control-allow-origin': '*',
+				'content-type': 'text/plain; charset=utf-8',
+			},
+		});
 	}
 }
